d856c0306b08c9a00af5725e7cdabe119e7040b1
Leon Mahardika <jaseoto@gmail.com>
2022-10-24 22:59:01 +0700

    Initial commit

---
+++ kotlin/id/infinitelearning/KotlinSubmission/exercise1/App.kt
@@ -0,0 +1,83 @@
+package id.infinitelearning.KotlinSubmission.exercise1
+
+/**
+Latihan 1
+Lengkapi fungsi myProfile di bawah ini dengan membuat variabel dengan ketentuan dibawah ini:
+- Variable bertipe data string yang menyimpan nilai nama depan Anda.
+- Variable bertipe data string yang menyimpan nilai nama belakang Anda.
+- Variable bertipe data number yang menyimpan nilai umur Anda.
+- Variable bertipe data boolean yang menyimpan nilai status Anda (single atau tidak)
+Dan cetak setiap variabel ke layar saat variable myProfile di panggil
+ */
+fun myProfile() {
+    var n1: String = "Petrus"
+    var n2: String = "Leon"
+    var umur: Int = 20
+    var status: Boolean = true
+    if (status == true){
+        println("Nama   : $n1 $n2")
+        println("Umur   : $umur Tahun")
+        println("Status : Single")
+    }
+}
+
+
+/**
+ *  Latihan 2
+ *  Lengkapi fungsi di bawah ini agar dapat mencetak nilai dari parameter-parameter yang ada dengan fungsi println
+ */
+fun groupDetail(groupId: String, groupMember: List<Any>, session: String): Any {
+    println(groupId)
+    println(groupMember)
+    println(session)
+    return arrayOf(groupId, groupMember, session)
+}
+
+
+/**
+ * Latihan 3
+ * Buat variable yang berisi daftar anggota group kamu,
+ * Kemudian akses item yang berisi nama Anda dari variable tersebut, lalu jadikan nilai kembalian untuk fungsi myName ini
+ *
+ */
+fun myName(): String {
+    var Anggota1: String = "Rofi"
+    var Anggota2: String = "Megy"
+    var Anggota3: String = "Leon"
+    var Anggota4: String = "Lia"
+    var Anggota5: String = "Tasya";
+    return "$Anggota3"
+}
+
+/**
+ * Latihan 4
+ * Ubah nilai kembalian untuk fungsi totalMember ini dengan rumus:
+ *
+ *      total mentor + jumlah anggota group
+ *
+ */
+fun totalMember(): Int {
+    val mentor = arrayOf("Dian", "Sani", "Ardy", "Reza")
+    val countOfGroup = 5
+
+    return mentor.count() + countOfGroup
+
+}
+
+fun main() {
+
+    myProfile()
+
+    val myName = myName()
+    println("My Name is: $myName")
+
+    val totalMember = totalMember()
+    println("Total Member group + Mentor: $totalMember")
+
+    /**
+     *  Latihan 5
+     *  Ubah nilai argumen-argumen dari fungsi groupDetail di bawah ini sesuai dengan data group kamu
+     *
+     */
+    groupDetail("Group 1",listOf("5 member", "Infinite Learning", "Lifestyle and Fashion"), "Siang")
+}
---
+++ kotlin/id/infinitelearning/KotlinSubmission/exercise4/App.kt
@@ -0,0 +1,29 @@
+package id.infinitelearning.KotlinSubmission.exercise4
+
+fun main() {
+
+    /**
+     * Buatlah contoh code penggunaan try catch untuk menangani suatu kasus sesuai keinginan teman-teman
+     *
+     */
+    try {
+        val result = validateAccount(name = "Leon", password = "012345")
+        println(result)
+    } catch ( error: Exception) {
+        println(error)
+    } finally {
+        println("Finish")
+    }
+}
+
+fun validateAccount(name: String, password: String): String {
+    if (name.count() < 3) {
+        throw Exception("Nama harus lebih panjang dari 3 huruf")
+    }
+
+    if (password.count() < 5) {
+        throw Exception("Password harus lebih panjang dari 5 huruf")
+    }
+
+    return "Selamat account anda telah terbuat !"
+}
